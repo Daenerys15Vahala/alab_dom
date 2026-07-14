1840e13686343a22f69f079ce2f0e6c1a4190382
Valentina BM <valentina15bermo@gmail.com>
2026-07-13 21:57:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,8 +12,16 @@ mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
 // add class
 mainEl.classList.add("flex-ctr");
 
+
+
 // menu bar
 const topMenuEl = document.querySelector("#top-menu");
 
 // height
-topMenuEl.style.height = 100;
+topMenuEl.style.height = "100%";
+
+// background menu bar
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+// add class
+topMenuEl.classList.add("flex-around");
